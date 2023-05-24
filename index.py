9768724b4def957d1ea7d24246e91bc632151403
Catalina Perez <catalinaperezbustos@gmail.com>
2023-05-24 22:36:23 +0200

    agregado acorde 1 pasión

--- index.py
+++ index.py
@@ -1,11 +1,36 @@
-acorde_deseado = input("POR FAVOR ELIJA DE LA SIGUIENTE LISTA DE EMOCIONES LA QUE DESEARÍA REPRESENTAR EN PINTURA, COMBINACIÓN DE VESTUARIO, ESCENA O IMPRESO: PASIÓN, CIENCIA, DEPORTE, CONFIANZA, DESCANSO, CALOR O ALEGRÍA")
+acorde_deseado = input("POR FAVOR ELIJA DE LA SIGUIENTE LISTA DE EMOCIONES LA QUE DESEARÍA REPRESENTAR EN PINTURA, COMBINACIÓN DE VESTUARIO, ESCENA O IMPRESO: PASIÓN, CIENCIA, DEPORTE, CONFIANZA, DESCANSO, CALOR O ALEGRÍA: ➡️  ")
 acorde_deseado = acorde_deseado.lower()
 acorde_deseado = acorde_deseado.upper()
 
-lista_de_colores = ["AMARILLO","AZUL","ROJO","BLANCO","NEGRO","GRIS","PLATEADO","NARANJA","VERDE","MARRÓN"]
-porcentaje = 12.5
+print(acorde_deseado)
 
+colores = ["AMARILLO","AZUL","ROJO","VERDE","NARANJA","NEGRO","BLANCO","PLATA","VIOLETA","GRIS"]
+emociones =["PASIÓN", "CIENCIA", "DEPORTE", "CONFIANZA", "DESCANSO", "CALOR", "ALEGRÍA"]
 
-lista_de_acordes = [
-    lista_acorde_pasion = []
+print(acorde_deseado in emociones)
+
+acorde_pasion = [
+    {
+        'COLOR':"ROJO",
+        'PORCENTAJE':"1/2"
+                     
+    },
+    {
+        'COLOR':"NARANJA",
+        'PORCENTAJE':"1/4"        
+    },
+    {
+        'COLOR':"VIOLETA",
+        'PORCENTAJE':"1/8"
+                     
+    },
+    {
+        'COLOR':"AMARILLO",
+        'PORCENTAJE':"1/8"
+                     
+    },
+  
 ]
+
+if acorde_deseado == "PASIÓN":
+    print(acorde_pasion)
