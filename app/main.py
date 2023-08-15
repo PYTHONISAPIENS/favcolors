2bf7198f6b70de62e25c83708803731603ca3642
Catalina Perez <catalinaperezbustos@gmail.com>
2023-08-15 11:10:42 +0200

    agos 15

---
+++ app/main.py
@@ -0,0 +1,10 @@
+import mod
+
+keys,values = mod.get_population()
+print(keys,values)
+
+print(mod.A)
+print(mod.B)
+print(mod.c)
+print(mod.cc)
+print(mod.bb)
---
+++ app/mod.py
@@ -0,0 +1,10 @@
+def get_population():
+    keys = ["col","bol"]
+    values = [300, 400]
+    return keys,values
+
+A= "hola gatito"
+B="HOLA PERRITO"
+c="hola gato gordo"
+cc="hola gordisimo"
+bb="hola bb"
